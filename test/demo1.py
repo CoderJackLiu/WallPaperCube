1f570d38dc55b34b85d9155b8a8511713003485c
Liuqi <596770421@qq.com>
2024-12-12 16:49:25 +0800

    多语言支持

--- test/demo1.py
+++
@@ -1,5 +0,0 @@
-
-if __name__ == "__main__":
-    print("demo1.py main")
-elif __name__ == "demo1":
-    print("demo1.py demo1")
--- test/demo2.py
+++
@@ -1,6 +0,0 @@
-import demo1
-
-
-
-if __name__ == "__main__":
-    print("demo2.py main  hello world!")
